0c679b532e00339f96efd9e065a27277c0742694
Miguel <mareyes2132@gmail.com>
2020-11-12 08:43:01 +0800

    last nagyd

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/filter-modified/'
-  : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  // ? '/filter-modified/'
+  // : '/'
 }
